f9418c51e26fbd525982399d7ce110adeb55fc78
Jimmi08 <janamicek@gmail.com>
2022-09-06 15:19:01 +0200

    news grid  template

--- templates/news/news_grid_template.php
+++ templates/news/news_grid_template.php
@@ -7,42 +7,14 @@
  * Released under the terms and conditions of the
  * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
  */
-$NEWS_GRID_TEMPLATE['blog-01']['start'] = '<section class="blog-area pb-5">
-   <div class="container">
-      <div class="row justify-content-center">';
 
-$NEWS_GRID_TEMPLATE['blog-01']['item'] =
-	'          <div class="col-lg-4 col-md-8 col-sm-10">
-            <div class="single-blog blog-style-one">
-              <div class="blog-image">
-                {SETIMAGE: w=400&h=400&crop=1}{NEWSTHUMBNAIL=placeholder}
-                <a href="javascript:void(0)" class="category">{NEWS_CATEGORY_NAME}</a>
-              </div>
-              <div class="blog-content">
-                <h5 class="blog-title">
-                  {NEWS_TITLE}
-                </h5>
-                <span><i class="lni lni-calendar"></i> {NEWS_DATE=short}</span>
-                <span>{NEWSCOMMENTS}</span>
-                <p class="text">
-                 {NEWS_SUMMARY}
-                </p>
-                <a class="more" href="{NEWSURL}">{LAN=READ_MORE}</a>
-              </div>
-            </div>
-            <!-- single blog -->
-          </div>';
 
-$NEWS_GRID_TEMPLATE['blog-01']['featured'] = $NEWS_GRID_TEMPLATE['blog-01']['item'];
-
-$NEWS_GRID_TEMPLATE['blog-01']['end'] = '</div></div></section>';
-
-$NEWS_GRID_TEMPLATE['blog-02']['start'] = '    <section class="blog-area pb-5">
+$NEWS_GRID_TEMPLATE['col-md-4']['start'] = '    <section class="blog-area pb-5">
 	<div class="container">
 	  <div class="row justify-content-center">';
 
-$NEWS_GRID_TEMPLATE['blog-02']['item'] =
-	'          <div class="col-lg-4 col-md-8 col-sm-10">
+$NEWS_GRID_TEMPLATE['col-md-4']['item'] =
+'          <div class="col-lg-4 col-md-8 col-sm-10">
 	<div class="single-blog blog-style-two">
 	  <div class="blog-image">
 	  {SETIMAGE: w=700&h=500&crop=1}{NEWSTHUMBNAIL=placeholder}
@@ -54,7 +26,7 @@ $NEWS_GRID_TEMPLATE['blog-02']['item'] =
 			</a>
 			</h4>
 		<span><i class="lni lni-calendar"></i> {NEWS_DATE=short}</span>
-		<span>{NEWSCOMMENTS}</span>
+		<span><i class="lni lni-comments-alt"></i> {NEWSCOMMENTS}</span>
 		<p class="text">{NEWS_SUMMARY}</p>
 		<a
 		  class="btn primary-btn-outline rounded-full"
@@ -66,55 +38,19 @@ $NEWS_GRID_TEMPLATE['blog-02']['item'] =
 	<!-- single blog -->
   </div>';
 
-$NEWS_GRID_TEMPLATE['blog-02']['featured'] = $NEWS_GRID_TEMPLATE['blog-02']['item'];
+$NEWS_GRID_TEMPLATE['col-md-4']['featured'] = $NEWS_GRID_TEMPLATE['col-md-4']['item'];
 
-$NEWS_GRID_TEMPLATE['blog-02']['end'] = '</div></div></section>';
-
-// Blog Style 5
-$NEWS_GRID_TEMPLATE['blog-05']['start'] =
-'<section class="blog-area pb-5">
-	<div class="container">
-	  <div class="row">{SETIMAGE: w=700&h=500&crop=1}';
-
-$NEWS_GRID_TEMPLATE['blog-05']['item'] =
-'<div class="col-lg-6">
-	<div class="single-blog blog-style-five d-sm-flex">
-	  <div class="blog-image">
-		<a href="{NEWSURL}"><img src="{NEWS_IMAGE: type=src&placeholder=true}" alt="{NEWS_TITLE}"/></a>
-	  </div>
-	  <div class="blog-content media-body">
-			<h4 class="blog-title">
-			<a href="{NEWSURL}">
-			{NEWS_TITLE}
-			</a>
-			</h4>
-		<span><i class="lni lni-calendar"></i> {NEWS_DATE=short}</span>
-		<span>{NEWSCOMMENTS}</span>
-		<p class="text">{NEWS_SUMMARY}</p>
-		<a
-		  class="more"
-		  href="{NEWSURL}"
-		  >{LAN=READ_MORE}</a
-		>
-	  </div>
-	</div>
-</div>';
-
-$NEWS_GRID_TEMPLATE['blog-05']['featured'] = $NEWS_GRID_TEMPLATE['blog-05']['item'];
-
-$NEWS_GRID_TEMPLATE['blog-05']['end'] = '</div></div></section>';
-
-
+$NEWS_GRID_TEMPLATE['col-md-4']['end'] = '</div></div></section>';
 
 // Blog Style 10 mini blog version one
-$NEWS_GRID_TEMPLATE['blog-10']['start'] =
+$NEWS_GRID_TEMPLATE['miniblog']['start'] =
 	'<section class="blog-area pb-5">
 	<div class="container">
 	  <div class="row">{SETIMAGE: w=700&h=500&crop=1}';
 
-$NEWS_GRID_TEMPLATE['blog-10']['item'] =
-	'<div class="col-lg-6">
-	<div class="single-blog blog-style-five d-sm-flex">
+$NEWS_GRID_TEMPLATE['miniblog']['item'] =
+	'<div class="col-lg-4 col-md-6 col-sm-10">
+	<div class="single-mini-blog d-flex">
 	  <div class="blog-image">
 		<a href="{NEWSURL}"
 		><img src="{NEWS_IMAGE: type=src&placeholder=true}" alt="{NEWS_TITLE}"
@@ -128,7 +64,6 @@ $NEWS_GRID_TEMPLATE['blog-10']['item'] =
 			</h4>
 		<span><i class="lni lni-calendar"></i> {NEWS_DATE=short}</span>
 		<span>{NEWSCOMMENTS}</span>
-		<p class="text">{NEWS_SUMMARY}</p>
 		<a
 		  class="more"
 		  href="{NEWSURL}"
@@ -138,6 +73,7 @@ $NEWS_GRID_TEMPLATE['blog-10']['item'] =
 	</div>
 </div>';
 
-$NEWS_GRID_TEMPLATE['blog-10']['featured'] = $NEWS_GRID_TEMPLATE['blog-05']['item'];
+$NEWS_GRID_TEMPLATE['miniblog']['featured'] = $NEWS_BLOGS_TEMPLATE['blog-10']['item'];
 
-$NEWS_GRID_TEMPLATE['blog-10']['end'] = '</div></div></section>';
+$NEWS_GRID_TEMPLATE['miniblog']['end'] = '</div></div></section>';
+ 
