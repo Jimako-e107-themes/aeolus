ae87dd1b06378bf2858eb117f57a0b9ea38c9408
Jimmi08 <janamicek@gmail.com>
2022-09-27 15:46:30 +0200

    better checks for js stuff

--- js/backtotop.js
+++ js/backtotop.js
@@ -4,17 +4,19 @@
     Sticky
     ======================================= */
     window.onscroll = function () {
-       
+
         // show or hide the back-top-top button
         var backToTo = document.querySelector(".scroll-top");
-        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-            backToTo.style.display = "flex";
-        } else {
-            backToTo.style.display = "none";
+        if (backToTo) {
+            if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+                backToTo.style.display = "flex";
+            } else {
+                backToTo.style.display = "none";
+            }
         }
     };
 
-     
+
     "use strict";
 
-}) ();
+})();
--- js/stickynavbar.js
+++ js/stickynavbar.js
@@ -5,14 +5,16 @@
     ======================================= */
     window.onscroll = function () {
         var header_navbar = document.querySelector(".navbar-area");
-        var sticky = header_navbar.offsetTop;
 
-        if (window.pageYOffset > sticky) {
-            header_navbar.classList.add("sticky");
-        } else {
-            header_navbar.classList.remove("sticky");
+        if (header_navbar) {
+            var sticky = header_navbar.offsetTop;
+
+            if (window.pageYOffset > sticky) {
+                header_navbar.classList.add("sticky");
+            } else {
+                header_navbar.classList.remove("sticky");
+            }
         }
-
     };
 
     "use strict";
