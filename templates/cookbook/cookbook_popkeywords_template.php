9b7d555c39c2e9dc6e01b25106a1810a70431c3f
Jimako <janamicek@gmail.com>
2022-12-19 13:23:44 +0100

    popkeywords added

---
+++ templates/cookbook/cookbook_popkeywords_template.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * CookBook - an e107 plugin by Tijn Kuyper (http://www.tijnkuyper.nl)
+ *
+ * Released under the terms and conditions of the
+ * Apache License 2.0 (see LICENSE file or http://www.apache.org/licenses/LICENSE-2.0)
+ *
+ * Popular keywords menu template
+*/
+
+if (!defined('e107_INIT')) { exit; }
+
+$COOKBOOK_POPKEYWORDS_TEMPLATE = array();
+ 
+
+$COOKBOOK_POPKEYWORDS_TEMPLATE['default']['caption'] = '{COOKBOOK_POPKEYWORDSMENU_CAPTION}';
+$COOKBOOK_POPKEYWORDS_TEMPLATE['default']['item']    = '<a href="{URL}" title="{KEYWORD}"><span class="btn btn-outline-primary my-1">{KEYWORD}</span></a>';
+
+$COOKBOOK_POPKEYWORDS_TEMPLATE['default']['start']   = '<div class="cookbook_popkeywords_menu">';
+$COOKBOOK_POPKEYWORDS_TEMPLATE['default']['body']    = "
+<div class='row'>
+    <div class='col-md-12'>
+        {COOKBOOK_POPULAR_KEYWORDS: limit=5} 
+    </div>
+</div>
+";
+$COOKBOOK_POPKEYWORDS_TEMPLATE['default']['end']     = '</div>';
